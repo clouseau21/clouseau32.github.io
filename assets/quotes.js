d714f99fd63415d2daf3b0c4a2df63b2bead58c8
clouseau21 <you@example.com>
2020-05-09 15:32:42 +0700

    initial commit

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,16 +1,5 @@
 // prettier-ignore
 export default [
-    arrbulan = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
-    date = new Date();
-    millisecond = date.getMilliseconds();
-    detik = date.getSeconds();
-    menit = date.getMinutes();
-    jam = date.getHours();
-    hari = date.getDay();
-    tanggal = date.getDate();
-    bulan = date.getMonth();
-    tahun = date.getFullYear();
-    document.write(tanggal+"-"+arrbulan[bulan]+"-"+tahun+"<br/>"+jam+" : "+menit+" : "+detik+"."+millisecond);
     { author: "Tandanghem", quotes: "Apapun yang terjadi, tetaplah bernafas" },
     { author: "Tandanghem", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "Tandanghem", quotes: "Hey! I wuv chuuuu! UwU" },
--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Rim"],
+  nicknames: ["Ling Ling"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
