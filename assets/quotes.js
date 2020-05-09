958ccc8034bcae226f190b73550f6e13bd8342da
clouseau21 <you@example.com>
2020-05-09 15:27:06 +0700

    initial commit

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,21 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    arrbulan = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+    date = new Date();
+    millisecond = date.getMilliseconds();
+    detik = date.getSeconds();
+    menit = date.getMinutes();
+    jam = date.getHours();
+    hari = date.getDay();
+    tanggal = date.getDate();
+    bulan = date.getMonth();
+    tahun = date.getFullYear();
+    document.write(tanggal+"-"+arrbulan[bulan]+"-"+tahun+"<br/>"+jam+" : "+menit+" : "+detik+"."+millisecond);
+    { author: "Tandanghem", quotes: "Apapun yang terjadi, tetaplah bernafas" },
+    { author: "Tandanghem", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Tandanghem", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Tandanghem", quotes: "I love you." },
+    { author: "Tandanghem", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Tandanghem", quotes: "I love you." },
+    { author: "Tandanghem", quotes: "I'm sorry for loving you." },
 ];
