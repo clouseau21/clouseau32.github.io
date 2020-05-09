e11507fb2bcf6ee1e76a274adeb19cac880b6a0d
clouseau21 <you@example.com>
2020-05-09 14:23:55 +0700

    initial commit

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Ling ling", "Cok", "Asw"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
